850b927de18c8624ccdc1705c4ea1439bd49b89a
Silviu Caragea <silviu.caragea@woowteam.com>
2018-12-03 17:06:58 +0200

    Replace the node ip only once after packet is installed

--- deployment/deb/postinst.sh
+++ deployment/deb/postinst.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+replace_var() {
+    variable_name=$1
+    variable_value=$2
+    file=$3
+    sed -i "s/^\(.*\)\${$variable_name}\(.*\)/\1$variable_value\2/" $file
+}
+
+ip=$(${get_ip_command})
+replace_var NODE_IP $ip ${config_file}
+
--- deployment/deb/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-APP=$1
-export NODE_IP=$(${get_ip_command})
-${APP} foreground > /dev/null 2>&1
