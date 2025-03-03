30c8e7ca1f80dc505d4f2e2b4960c0d0527e00bc
silviu <silviu.cpp@gmail.com>
2025-03-03 23:07:52 +0000

    Use systemd app as dependency

--- src/erl_deployment_sup.erl
+++ src/erl_deployment_sup.erl
@@ -12,9 +12,5 @@ start_link() ->
 
 init([]) ->
     Children = [
-        proccess(erl_systemd, infinity)
     ],
     {ok, {{one_for_one, 20, 1}, Children}}.
-
-proccess(Name, WaitForClose) ->
-    {Name, {Name, start_link, []}, permanent, WaitForClose, worker, [Name]}.
